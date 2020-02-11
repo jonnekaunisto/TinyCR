86774ec0f5bddc4381bdc3098ce286d3a37ee4e9
Jonne Kaunisto <jonneka@gmail.com>
2020-02-11 13:15:16 -0800

    deleted cpp

--- include/TinyCRClient.h
+++ include/TinyCRClient.h
@@ -10,12 +10,19 @@
 class TinyCRClient
 {
 public:
-    TinyCRClient(int serverIP);
+    TinyCRClient(int serverIP)
+    {
+        this->serverIP = serverIP;
+        CRIoT_Data_VO<uint64_t, uint32_t>daasClient();
+    }
 
     /**
      * Start client, get request from server and listen to updates
      */
-    bool startClient();
+    bool startClient()
+    {
+        requestInitialSummary();
+    }
 
     /**
      * Query a peer for certificate, returns bool
@@ -26,8 +33,92 @@ private:
     int serverIP;
     CRIoT_Data_VO<uint64_t, uint32_t> dassClient;
 
-    void requestInitialSummary();
-    void listenForSummaryUpdates();
+    void requestInitialSummary()
+    {
+        vector<uint8_t> msg;
+        try
+        {
+            /*initialize connnection to server*/
+            ClientSocket client_socket (serverIP, 30000 );
+            
+            try
+            {
+                /*recieve the CRC packets*/
+                while(true)
+                {
+                    char* data = new char [MAXRECV + 1];
+                    int n_bytes = client_socket.recv(data);
+                    // for(int k=7; k>=0; k--)
+                    // 	cout<<((data[0]>>k)&(uint8_t(1)))<<" ";
+                    // cout<<endl;
+                    for(int i=0; i<n_bytes; i++)
+                    {
+                        uint8_t byte;
+                        memcpy(&byte, &data[i], 1);
+
+                        msg.push_back(byte);
+                    }
+                    if(data[n_bytes-1]=='h' && data[n_bytes-2]=='s' && data[n_bytes-3]=='i' && data[n_bytes-4]=='n' 
+                        && data[n_bytes-5]=='i' && data[n_bytes-6]=='f')
+                        break;
+                    delete[] data;
+                }
+                
+            }
+            catch ( SocketException& ) {}
+
+
+        }
+        catch ( SocketException& e )
+        {
+            std::cout << "Exception was caught:" << e.description() << "\n";
+        }
+
+        /*decoding*/
+        daasClient.decoding(msg);
+    }
+    
+    void listenForSummaryUpdates()
+    {
+        std::cout << "Listening For Summary Updates...\n";
+        try
+        {
+            // Create the socket
+            ServerSocket server(40000);
+
+            while (true)
+            {
+
+                ServerSocket new_sock;
+                server.accept(new_sock);
+
+                try
+                {
+                    while (true)
+                    {
+                        std::string data;
+                        new_sock >> data;
+                        if (data == "Delta Summary")
+                        {
+                            new_sock << "Received Summary";
+                            std::cout << "Received Delta Summary\n";
+                        }
+                        else
+                        {
+                            new_sock << "Some error";
+                        }
+                    }
+                }
+                catch (SocketException &)
+                {
+                }
+            }
+        }
+        catch (SocketException &e)
+        {
+            std::cout << "Exception was caught:" << e.description() << "\nExiting.\n";
+        }
+    }
 };
 
 #endif
--- include/TinyCRServer.h
+++ include/TinyCRServer.h
@@ -10,15 +10,32 @@ template<typename K, class V>
 class TinyCRServer
 {
 public:
-    TinyCRServer(int port, vector<K> positive_keys, vector<V> negative_keys);
+    TinyCRServer(int port, vector<K> positive_keys, vector<V> negative_keys)
+    {
+        this->port = port;
+        this->positive_keys = positive_keys;
+        this->negative_keys = negative_keys;
+        CRIoT_Control_VO<uint64_t, uint32_t>daasServer(positive_keys, negative_keys);
+    }
+
     /**
      * Starts tiny CR server. Send delta updates on changes, monitor port for new devices
      */
-    bool startServer();
+    bool startServer()
+    {
+        std::thread connectionListenerThread(listenForNewDevices);
+        std::thread summaryUpdatesThread(sendSummaryUpdates);
+
+        summaryUpdatesThread.join();
+        connectionListenerThread.join();
+        return 0;
+    }
     /**
      * Starts socket server for taking in commands from 3rd party
      */
-    bool startSocketAPIServer();
+    bool startSocketAPIServer(){
+        return false;
+    }
 
 private:
     int port;
@@ -38,7 +55,93 @@ private:
      */
     int removeDevice(int device);
 
-    void listenForNewDevices();
-    void sendSummaryUpdates();
+    void listenForNewDevices()
+    {
+        try
+        {
+            ServerSocket server(port);
+
+            while (true)
+            {
+
+                ServerSocket new_sock;
+                server.accept(new_sock);
+                int msg_size = 0;
+
+                try
+                {
+                    while (true)
+                    {
+                        /*send the CRC packets*/
+                        vector<vector<uint8_t>> v = daasServer.encoding(daasServer.vo_data);
+                        for (int i = 0; i < v.size(); i++)
+                        {
+                            char *msg;
+                            msg = new char[v[i].size()];
+                            for (int j = 0; j < v[i].size(); j++)
+                            {
+                                memcpy(&msg[j], &v[i][j], 1);
+                            }
+                            new_sock.send(msg, v[i].size());
+
+                            msg_size += v[i].size();
+
+                            delete[] msg;
+                        }
+
+                        cout << "size: " << msg_size << endl;
+                        /*if finished, close*/
+                        new_sock << "finish";
+                        break;
+                    }
+                }
+                catch (SocketException &)
+                {
+                }
+            }
+        }
+        catch (SocketException &e)
+        {
+            std::cout << "Exception was caught:" << e.description() << "\nExiting.\n";
+        }
+    }
+
+
+    void sendSummaryUpdates()
+    {
+        while (true)
+        {
+            // add something to trigger a change
+            if (true)
+            { 
+                std::cout << "Sending Delta Summary...\n";
+                try
+                {
+                    char device[1][10] = {"localhost"};
+                    for (int i = 0; i < 1; i++)
+                    {
+                        ClientSocket client_socket(device[i], 40000);
+
+                        std::string reply;
+
+                        try
+                        {
+                            client_socket << "Delta Summary";
+                            client_socket >> reply;
+                        }
+                        catch (SocketException &)
+                        {
+                        }
+
+                        std::cout << "We received this response from the client:\n\"" << reply << "\"\n";
+                    }
+                }
+                catch (SocketException &e)
+                {
+                    std::cout << "Exception was caught:" << e.description() << "\n";
+                }
+            }
+        }
+    }
 };
 #endif
