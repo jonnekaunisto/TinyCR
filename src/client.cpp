9f67c0a309cdd8ea16decac5e82222378e0b5593
Jonne <username@gitlab.shouqianshi.space>
2020-03-14 11:38:53 -0700

    added interface for client

--- src/client.cpp
+++ src/client.cpp
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include "../include/ClientSocket.h"
 #include "../include/SocketException.h"
+#include <regex>
 #define COMMAND_PORT 60000
 
 TinyCRClient<uint64_t, uint32_t>client("localhost");
@@ -18,36 +19,45 @@ int main(int argc, char **argv)
 
 	std::thread clientThread (runClientThread);
 
+	ServerSocket server(COMMAND_PORT);
+	std::cout << "Listening For Commands on Port: " << COMMAND_PORT << "\n";
+	//implement something to take commands and stuff
+	std::regex rgx("show ([0-9]+)");
 
 	while(true)
 	{
+		try
+        {
+			ServerSocket new_sock;
+			server.accept(new_sock);
+			std::string data;
+            new_sock >> data;
 
-		//implement something to take commands and stuff
-		std::regex rgx("(show) ([0-9]+)");
-        ServerSocket server(COMMAND_PORT);
-        std::cout << "Listening For Commands on Port: " << COMMAND_PORT << "\n";
+			std::smatch matches;
+			if(!std::regex_search(data, matches, rgx)) 
+			{
+				new_sock << "Command not recognized";
+				continue;
+			
+			}
 
-		sleep(10); //sleep for 5 seconds
-		uint32_t num = 1;
-		bool v = client.queryCertificate(num);
-		if(v)
-		{
-			std::cout <<  "1 valid"<< "\n";
-		}
-		else
-		{
-			std::cout <<  "1 not valid"<< "\n";
-		}
+			std::string str_num = matches[1];
+			uint32_t num = stoul(str_num);
 
-		v = client.queryCertificate(100500);
-		if(v)
-		{
-			std::cout <<  "100500 valid"<< "\n";
+			bool v = client.queryCertificate(num);
+			if(v)
+			{
+				new_sock << str_num << " is valid"<< "\n";
+			}
+			else
+			{
+				new_sock << str_num << " is not valid"<< "\n";
+			}
 		}
-		else
+		catch (SocketException &e)
 		{
-			std::cout <<  "100500 not valid"<< "\n";
-		}
+			std::cout << "Exception was caught:" << e.description() << "\n";
+		}  
 	}
 
 	clientThread.join();
--- src/mockCA.cpp
+++ src/mockCA.cpp
@@ -1,3 +1,7 @@
+/**
+ * Simulates a Certificate Authority
+ * @author Jonne Kaunisto 
+ */
 #include "../include/ClientSocket.h"
 #include "../include/SocketException.h"
 #include <iostream>
@@ -9,14 +13,27 @@ std::string valid_commands = "valid commands are:\n"
                               "\"rev {k} \"\n"
                               "\"exi\"\n"; 
 
-int main ()
+/*
+ *
+ */
+int main (int argc, char *argv[])
 {
+    std::string host = "localhost";
+    int port = 50000;
+    if (argc == 3)
+    {
+        host = argv[1];
+        port = atoi(argv[2]);
+    }
+
+    std::cout << "Connecting to host: " << host << " port: " << port << "\n";
+
     std::cout << valid_commands;
     while(true)
     {
         try
         {
-            ClientSocket client_socket("localhost", 50000);
+            ClientSocket client_socket(host, port);
             std::string input;
             std::string response;
             std::cout << "Enter a command: ";
